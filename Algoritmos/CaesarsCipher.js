886c819149a076f7d736bb1f8c15f0a5b203939c
RicardoFJN <ricardo.nunes.isla@gmail.com>
2016-03-04 19:22:16 +0000

    Alteração ao algoritmo. A funcionar!

--- Algoritmos/CaesarsCipher.js
+++ Algoritmos/CaesarsCipher.js
@@ -26,83 +26,52 @@
 function rot13(str){
 
 
-    var charArray = [
-        [32, 33, 34, 35, 36 ,37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
-        [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77],
-        [78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
-    ];
+    var firstArray = [32, 33, 34, 35, 36 ,37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47];
+    var secondArray = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77];
+    var thirdArray = [78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
 
 
     var decipher = "";
 
 
+    //Verificar se a letra pertence ao array se sim então trocar a letra por outro array
 
-    for(var i = 0; i < charArray[2].length; i++){
-
-        //A funcionar
-
-        for (var j = 0; j < str.length; j++){
-            if(str.charCodeAt(j) === charArray[0][i]){
-                decipher += charArray[0][i] + " ";
-
-            }
-            if(str.charCodeAt(j) === charArray[1][i]){
-                decipher += charArray[1][i] + " ";
-            }
-            if (str.charCodeAt(j) === charArray[2][i]){
-                decipher += charArray[2][i] + " ";
-
+    for(var i = 0; i < str.length; i++){
+        for (var j = 0; j < firstArray.length; j++){
+            if (str.charCodeAt(i) === firstArray[j]){
+                //console.log("Pertence ao primeiro array");
+                decipher += String.fromCharCode(firstArray[j]);
             }
 
         }
-
-        //Segunda parte da verificação
-
-        /*for (var i = 0; i < decipher.length; i++){
-            for (var j = 0; j < charArray[2]; j++){
-                if (decipher[i] === charArray[2][j]){
-                    decipher[i] = charArray[1][j];
-                    break;
-                }
+        for (var k = 0; k < secondArray.length; k++){
+            if(str.charCodeAt(i) === secondArray[k]){
+                //console.log("Pertence ao segundo array");
+                decipher += String.fromCharCode(thirdArray[k]);
             }
-        }*/
+        }
+        for (var y = 0; y < thirdArray.length; y++){
+            if(str.charCodeAt(i) === thirdArray[y]){
+                //console.log("Pertence ao terceiro array");
 
+                    decipher += String.fromCharCode(secondArray[y]);
 
-
-
+            }
+        }
     }
 
 
 
 
-    /*for (var i in str){
-        for (var j in charArray){
-            if (str.charCodeAt(i) === charArray[0][j]){
-
-                decipher += String.fromCharCode(charArray[0][j]);
-
-            }if(str.charCodeAt(i) === charArray[1][j]){
-
-                decipher += String.fromCharCode(charArray[1][j]);
-
-
-            }if(str.charCodeAt(i) === charArray[2][j]){
-
-                        decipher += String.fromCharCode(charArray[1][j]);
-
-
-            }
-        }
-    }*/
-
-    //Problema é que o ciclo só vai 3 vezes
-
     return decipher;
 
-    //return charArray[1][5];
+
+    //A cifra troca sempre a posição independenmtente da letra que é por exemplo se for N então é igual a A se for A então é igual a N
+
+
 }
 
-console.log(rot13("SRR VN"));
+console.log(rot13("SERR PBQR PNZC"));
 console.log(rot13("SERR CVMMN!"));
-//console.log(rot13("SERR YBIR?"));
-//console.log(rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."));
+console.log(rot13("SERR YBIR?"));
+console.log(rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."));
